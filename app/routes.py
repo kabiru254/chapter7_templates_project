dd95bfaa315a602c2ef3acaae39e8aa8d29dd7aa
kabiru254 <xyrisyt@gmail.com>
2026-04-02 11:25:43 +0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
